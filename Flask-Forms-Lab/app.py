fbc04d78f9cc2677f102f85750f9b1c36bf9ca80
laviba23-meet <LaviBanner@gmail.com>
2022-07-27 14:04:28 +0300

    Completed

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -10,7 +10,7 @@ app = Flask(  # Create a flask app
 
 username = "LaviBanner"
 password = "Lavi080906"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["Farid","Geffen","Leena", "Zain", "Lourd", "Joud", "Jude"]
 
 
 @app.route('/', methods= ['GET','POST'])  # '/' for the default page
@@ -18,11 +18,22 @@ def login():
 	if request.method=='POST':
 		if username==request.form['username'] and password==request.form['password']:
 			return render_template('home.html')
-
-  return render_template('login.html')
+		else:
+			return render_template('login.html')
+	else:
+		return render_template('login.html')
   
 
+@app.route('/home')
+def home():
+	return render_template('home.html', friends=facebook_friends)
 
+@app.route('/friend_exists/<string:name>',methods=['GET','POST'])
+def friend_exists(name):
+	isfriend=False
+	if name in facebook_friends:
+		isfriend=True
+	return render_template('friend_exists.html', isfriend=isfriend)
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
