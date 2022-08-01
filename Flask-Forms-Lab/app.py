145b67749628c1fe6469cd3b0a97fb322050b3e8
laviba23-meet <LaviBanner@gmail.com>
2022-08-02 00:23:46 +0300

    Completed+bonus

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -2,40 +2,56 @@ from flask import Flask, jsonify, request, render_template, redirect, url_for
 import random
 
 app = Flask(  # Create a flask app
-	__name__,
+	_name_,
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
 
 
-username = "LaviBanner"
-password = "Lavi080906"
-facebook_friends=["Farid","Geffen","Leena", "Zain", "Lourd", "Joud", "Jude"]
+accounts={
+	"Lavi" : "123",
+	"Banner" : "456"
 
 
-@app.route('/', methods= ['GET','POST'])  # '/' for the default page
+}
+username = "Lavi"
+password1 = "123"
+facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+
+
+@app.route('/' ,methods=['GET','POST'])  # '/' for the default page
 def login():
-	if request.method=='POST':
-		if username==request.form['username'] and password==request.form['password']:
-			return render_template('home.html')
-		else:
-			return render_template('login.html')
-	else:
+	facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+	if request.method=='GET':
 		return render_template('login.html')
-  
+	else:
+		name = request.form['username']
+		password = request.form['password']
+		for x in (accounts):
+			if name==x and password==accounts[x]:
+				return render_template('home.html',name=name,password=password,facebook_friends=facebook_friends)
+		
+		return render_template('login.html')
 
-@app.route('/home',methods=['GET','POST'])
+@app.route('/home' ,methods=['GET','POST'])  # '/' for the default page
 def home():
-	return render_template('home.html', friends=facebook_friends)
 
-@app.route('/friend_exists/<string:name>',methods=['GET','POST'])
-def friend_exists(name):
-	isfriend=False
-	if name in facebook_friends:
-		isfriend=True
-	return render_template('friend_exists.html', isfriend=isfriend)
+  return render_template('home.html',facebook_friends=facebook_friends)
+@app.route('/friends_exist/<string:name>' ,methods=['GET','POST'])  # '/' for the default page
+def friends_exist(name):
+	y=0
+	exists= False
+	for x in (facebook_friends):
+		if name==x:
+			y+=1
+	if y>=1:
+		exists=True
+	return render_template('friend_exists.html',facebook_friends=facebook_friends,exists=exists,name1=name)
+		
 
-if __name__ == "__main__":  # Makes sure this is the main process
+
+
+if _name_ == "_main_":  # Makes sure this is the main process
 	app.run( # Starts the site
     debug=True
 	)
